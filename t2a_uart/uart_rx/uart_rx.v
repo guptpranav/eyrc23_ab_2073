2114aba2d080f6663a14525400e2f0ffe1283fd4
pranavgupta2003 <pranavgupta.53535@gmail.com>
2023-11-15 22:43:38 +0530

    uart_rx completed

--- t2a_uart/uart_rx/uart_rx.v
+++ t2a_uart/uart_rx/uart_rx.v
@@ -36,6 +36,106 @@ initial begin
     rx_msg = 0; rx_complete = 0;
 end
 
+// FSM States
+parameter START = 0, STOP = 9, IDLE = 10;
+parameter ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8;
+
+reg [7:0] rx_temp = 8'b0;
+reg [3:0] state = IDLE;
+reg [9:0] cycle = 10'b1111111111;
+reg [9:0] counter = 10'b0;
+
+always @(posedge clk_50M) begin
+	cycle = cycle+1;
+    case (state)
+		IDLE: begin
+			rx_complete <= 0;
+			if (!rx) begin
+				state <= START;
+			end
+		end
+		START: begin
+			rx_temp <= 0;
+			rx_complete <= 0;
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= ONE;
+			end
+			rx_temp[7] <= rx;
+		end
+		ONE: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= TWO;
+			end
+			rx_temp[6] <= rx;
+		end
+		TWO: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= THREE;
+			end
+			rx_temp[5] <= rx;
+		end
+		THREE: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= FOUR;
+			end
+			rx_temp[4] <= rx;
+		end
+		FOUR: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= FIVE;
+			end
+			rx_temp[3] <= rx;
+		end
+		FIVE: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= SIX;
+			end
+			rx_temp[2] <= rx;
+		end
+		SIX: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= SEVEN;
+			end
+			rx_temp[1] <= rx;
+		end
+		SEVEN: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= EIGHT;
+			end
+			rx_temp[0] <= rx;
+		end
+		EIGHT: begin
+			if (cycle == 434) begin
+				cycle <= 0;
+				state <= STOP;
+			end
+		end
+		STOP: begin
+			if (!rx) begin
+				counter <= counter+1;
+			end
+			if (counter >= 433) begin
+				rx_msg <= 0;
+				state <= IDLE;
+			end else if (cycle == 434) begin
+				cycle <= 0;
+				counter <= 0;
+				state <= IDLE;
+				rx_msg <= rx_temp;
+				rx_complete <= 1;
+			end
+		end
+	endcase
+end
+
 //////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE//////////////////
 
 endmodule
